6a2a703e10ff9a6c5bbf0dbf451f7a71c97dc734
Gangana3 <organy110@gmail.com>
2018-06-11 13:14:20 +0300

    Adding files

---
+++ TcpChat1/ChatPage.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+
+namespace TcpChat1
+{
+    /// <summary>
+    /// Interaction logic for ChatPage.xaml
+    /// </summary>
+    public partial class ChatPage : Page
+    {
+        private static readonly object lockObj = new object();
+
+
+        public event EventHandler ConnectionClosed;   // After connection is closed
+
+
+        protected virtual void OnConnectionClosed()
+        {
+            if (this.ConnectionClosed != null)
+                this.ConnectionClosed(this, EventArgs.Empty);
+        }
+
+
+        public ChatPage()
+        {
+            InitializeComponent();
+
+
+            RoutedEventHandler ReceiveMessages = delegate (object sender, RoutedEventArgs e)
+            {
+                // Clear the chat box from previous conversations
+                this.chatTextBox.Text = "";
+
+                // Change the header
+                this.header.Content = string.Format("Connected to {0}", GlobalData.user.FriendIP);
+
+                // Receive messages in a different thread
+                var receiveMessagesThread = new Thread(
+                    delegate ()
+                    {
+                        var messageSound = new System.Media.SoundPlayer(@"Resources/message.wav");
+                        try
+                        { 
+                            while (true)
+                            {
+                                Message message = GlobalData.user.Receive();  // Receive message
+                                
+                                // Print the received message and play a message sound
+                                this.AppendChatTextBox(string.Format("[{0}] {1}: {2}\n", message.TimeFormed, message.Sender.Username, message.Content));
+                                messageSound.Play();
+                            }
+                        }
+                        catch (System.IO.IOException)
+                        {
+                            this.Dispatcher.Invoke(() =>
+                            {
+                                MessageBox.Show("The other user has left the chatroom!");
+                                GlobalData.user.CloseConnection();
+                                this.OnConnectionClosed();
+
+                            });
+                        }
+                    });
+                receiveMessagesThread.IsBackground = true;
+                receiveMessagesThread.Start();
+            };
+
+            this.Loaded += ReceiveMessages;     // When the page is loaded, start receiving messages
+
+            // When a key is down
+            this.KeyDown += delegate (object sender, KeyEventArgs e)
+            {
+                if (e.Key == Key.Enter)
+                {
+                    // When the enter key is down, press the send button
+                    this.sendButton_Click(sender, new RoutedEventArgs());
+                }
+            };
+        }
+
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.messageTextBox.Text))
+            {
+                MessageBox.Show("Message cannot be empty!");
+            }
+            else
+            {
+                // Send the message
+                GlobalData.user.Send(this.messageTextBox.Text);
+
+                // Print the message
+                var time = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.Local);
+                int minute = time.Minute;
+                int hour = time.Hour;
+                string minuteString, hourString;
+                if (minute < 10)
+                    minuteString = "0" + minute.ToString();
+                else
+                    minuteString = minute.ToString();
+                if (hour < 10)
+                    hourString = "0" + minute.ToString();
+                else
+                    hourString = hour.ToString();
+                this.AppendChatTextBox(string.Format("[{0}:{1}] YOU: {2}\n", time.Hour, time.Minute, this.messageTextBox.Text));
+
+                // Empty the text box
+                this.messageTextBox.Text = string.Empty;
+            }
+        }
+
+
+        /// <summary>
+        /// Appends the Chat Text block on the page with the given message
+        /// </summary>
+        /// <param name="rawMessage">Message to print to the text block</param>
+        private void AppendChatTextBox(string rawMessage)
+        {            
+            this.Dispatcher.Invoke(() => { this.chatTextBox.Text += rawMessage; this.scrollBar.ScrollToEnd();  });            
+        }
+    }
+}
---
+++ TcpChat1/User.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TcpChat1
+{
+    class User
+    {
+        const int TimeBetweenConnectionAttempts = 1000;  // In Miliseconds
+        const int DefaultBufferSize = 1024;              // In Bytes
+
+        private string username;
+        private string friendIP;
+        private int port;
+        private int friendPort;
+        private TcpListener tcpListener;
+        private TcpClient outputTcpClient;
+        private TcpClient inputTcpClient;
+        public event EventHandler Connected;
+
+
+        protected virtual void OnConnected()
+        {
+            if (this.Connected != null)
+                Connected(this, EventArgs.Empty);
+        }
+
+
+        public User(string username, string friendIP, int port, int friendPort)
+        {
+            this.username = username;
+            this.friendIP = friendIP;
+            this.port = port;
+            this.friendPort = friendPort;
+            this.tcpListener = new TcpListener(new IPEndPoint(IPAddress.Any, this.port));
+            this.outputTcpClient = new TcpClient();
+        }
+
+
+        public string Username { get { return this.username; } }
+        public string FriendIP { get { return this.friendIP; } }
+        public int Port { get { return this.port; } }
+        public int FriendPort { get { return this.friendPort; } }
+        private NetworkStream OutputStream { get { return this.outputTcpClient.GetStream(); } }
+        private NetworkStream InputStream { get { return this.inputTcpClient.GetStream(); } }
+
+
+        /// <summary>
+        /// Connect to the other client
+        /// </summary>
+        public void Connect()
+        {
+            this.tcpListener.Start();
+            bool isConnected = false;
+            while (!isConnected)
+            {
+                isConnected = this.TryToConnect();
+                if (!isConnected) Thread.Sleep(TimeBetweenConnectionAttempts);
+            }
+            this.OnConnected();
+        }
+
+
+        /// <summary>
+        /// Try to connect to the other user
+        /// </summary>
+        /// <returns> Whether connected to the other user or not </returns>
+        private bool TryToConnect()
+        {
+            try
+            {
+                this.outputTcpClient.Connect(new IPEndPoint(IPAddress.Parse(this.FriendIP), this.FriendPort));
+
+                const int attempts = 3;
+                for (int i = 0; i < attempts; i++)
+                {
+                    if (this.tcpListener.Pending())
+                    {
+                        this.inputTcpClient = tcpListener.AcceptTcpClient();
+                        // In case connected successfully without throwing an exception
+                        return true;
+                    }
+                    Thread.Sleep(TimeBetweenConnectionAttempts);
+                }
+                throw new Exception("Something went wrong!");   // In case all attempts failed
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+
+        }
+
+
+        /// <summary>
+        /// Send a message to the other user
+        /// </summary>
+        /// <param name="message">message to send</param>
+        public void Send(string messageContent)
+        {
+            var message = new Message(messageContent, this);
+            byte[] buffer = Encoding.UTF8.GetBytes(message.ToString());
+            this.OutputStream.Write(buffer, 0, buffer.Length);
+        }
+
+
+        /// <summary>
+        /// Receives a message from the other user
+        /// </summary>
+        /// <returns> The message from the other user </returns>
+        public Message Receive()
+        {
+            return new Message(ReadFromStream(this.InputStream));
+        }
+
+
+        /// <summary>
+        /// Closes the connection with the other user
+        /// </summary>
+        public void CloseConnection()
+        {
+            this.tcpListener.Stop();
+            this.inputTcpClient.Close();
+            this.inputTcpClient.Close();
+        }
+
+
+        /// <summary>
+        /// Read data from given network stream
+        /// </summary>
+        /// <param name="stream">Stream to read from</param>
+        /// <returns>The stream data</returns>
+        private static string ReadFromStream(NetworkStream stream)
+        {
+            var streamData = new StringBuilder();
+            var buffer = new byte[DefaultBufferSize];
+
+            do
+            {
+                int dataLength = stream.Read(buffer, 0, buffer.Length);
+                streamData.Append(Encoding.UTF8.GetString(buffer, 0, dataLength));
+            } while (stream.DataAvailable);
+
+            return streamData.ToString();
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("{0}: My port -> {1}, Friend's port -> {2}, Friend's IP Address -> {3}", this.Username, this.Port, this.FriendPort, this.FriendIP);
+        }
+    }
+}
